a194f53c1f9ed7b0eb1a137495e0f83cac637995
Richard Miao <richardmiao1111@gmail.com>
2023-01-21 16:24:14 -0500

    test

--- Screens/MapViewScreen.js
+++ Screens/MapViewScreen.js
@@ -4,12 +4,30 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import MapView from 'react-native-maps';
 import { Marker } from 'react-native-maps';
 import './marker.png';
+import { GetCurrentPosition } from '../Scripts/location';
 
 export default function MapViewScreen(navigation) {
+
+    const [longitude, setLongitude] = React.useState(43.660816376640064);
+    const [latitude, setLatitude] = React.useState(-79.39662293097538);
+
+    React.useEffect(() => {
+        GetCurrentPosition()
+        .then(data => {
+            setLongitude(data.coords.longitude);
+            setLatitude(data.coords.latitude);
+        }, [])
+    })
+    //43.660816376640064, -79.39662293097538
+    
     return (
         <SafeAreaView style={ styles.container }>
             <MapView style={ styles.map }>
 
+                {/* location: user */}
+                <Marker coordinate={{ latitude: latitude, longitude : longitude}}>
+                    <Image source={require("./marker.png")} style={ styles.userLocation } />
+                </Marker>
                 {/* location: uoft convocation hall */}
                 <Marker coordinate={{ latitude : 43.66092400242462, longitude : -79.3951897865203}}>
                     <Image source={require("./marker.png")} style={ styles.washroomLocations } />
@@ -36,6 +54,10 @@ const styles = StyleSheet.create({
       height: '100%',
     },
     washroomLocations: {
+        width: 10,
+        height: 10
+    },
+    userLocation: {
         width: 10,
         height: 10
     }
