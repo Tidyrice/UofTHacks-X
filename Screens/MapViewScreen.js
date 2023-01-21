d19543414070df844181a45b39d569695ea66116
Richard Miao <richardmiao1111@gmail.com>
2023-01-21 12:03:47 -0500

    location markers functions in MapViewScreen.js

--- Screens/MapViewScreen.js
+++ Screens/MapViewScreen.js
@@ -1,19 +1,29 @@
-import { Text, StyleSheet } from "react-native";
+import * as React from 'react';
+import { Text, StyleSheet, Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import MapView from 'react-native-maps';
 import { Marker } from 'react-native-maps';
+import './marker.png';
 
 export default function MapViewScreen(navigation) {
     return (
-        <SafeAreaView style={styles.container}>
-            <MapView style={styles.map} />
+        <SafeAreaView style={ styles.container }>
+            <MapView style={ styles.map }>
+ 
+                <Marker coordinate={{ latitude : 43.66092400242462, longitude : -79.3951897865203}}>
+                    <Image source={require("./marker.png")} style={{width: 10, height: 10}} />
+                </Marker>
+
+            </MapView>
         </SafeAreaView>
+        
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
+      padding: 10,
+      paddingTop: -6
     },
     map: {
       width: '100%',
