45afccf64409ff4933f35dea4d8775c79ef4515d
Cheney Cai <cheneycai2004@hotmail.com>
2023-01-21 09:45:50 -0500

    timer stuff

--- Screens/TimerScreen.js
+++ Screens/TimerScreen.js
@@ -1,7 +1,46 @@
-import { Text } from "react-native";
+import { StyleSheet, Pressable, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function TimerScreen(navigation) {
     return (
-        <Text>TIMER SCREEN</Text>
+        <SafeAreaView style = {styles.view}>
+
+            <Text style = {styles.text}>
+                00:00
+            </Text>
+
+            <Pressable style = {({pressed}) => [
+                {
+                    opacity: pressed ? 0.3 : 1,
+                },
+                styles.pressable,
+            ]}>
+
+                <Text style = {styles.pressableText}>
+                    Start Timer!
+                </Text>
+            </Pressable>
+
+        </SafeAreaView>
     );
 }
+
+const styles = StyleSheet.create({
+    view: {
+        alignItems: "center",
+    },
+    text: {
+        fontWeight: "bold",
+        fontSize: 32,
+        padding: 8,
+    },
+    pressable: {
+        borderColor: "black",
+        borderWidth: 4,
+        padding: 8,
+    },
+    pressableText: {
+        fontWeight: "bold",
+        fontSize: 48,
+    },
+});
